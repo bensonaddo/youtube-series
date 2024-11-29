11fb512c188594ee0d36035eb7986b24e5221ecc
snipersgee <snip@snip.com>
2024-11-29 17:05:53 -0600

    added css styles to todo button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ const Todo = () => {
   return (
     <div className="card">
       <h2>Title</h2>
-      <div>
+      <div className="actions">
         <button className="btn">Delete</button>
       </div>
     </div>
