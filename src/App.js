536de7c4c54740f2015131a89e455946b3beba62
snipersgee <snip@snip.com>
2024-11-29 16:34:49 -0600

    Added html and css styles to intial page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <div className="card">
+        <h2>Title</h2>
+        <div>
+          <button className="btn">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
