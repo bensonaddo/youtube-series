2d706f0fde20421b928d3946fc413b95b9eb6ed6
snipersgee <snip@snip.com>
2024-11-29 16:44:37 -0600

    added todo components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
-      <div className="card">
-        <h2>Title</h2>
-        <div>
-          <button className="btn">Delete</button>
-        </div>
-      </div>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+const Todo = () => {
+  return (
+    <div className="card">
+      <h2>Title</h2>
+      <div>
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
