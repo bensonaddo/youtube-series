660b7e9471272b36c7d2d00f579ebf5d390dd052
snipersgee <snip@snip.com>
2024-12-02 13:38:45 -0600

    Updated favorite page to render fav from state object

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+
+import FavoritesContext from "../state-mgmt/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function Favorites() {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
     <div>
       <h1>Favorites</h1>
+      <MeetupList meetups={favoritesCtx.favorites} />
     </div>
   );
 }
