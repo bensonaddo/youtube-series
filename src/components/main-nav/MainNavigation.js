351de95c82445809956b2dfb710db94e757f6c8e
snipersgee <snip@snip.com>
2024-12-02 13:50:51 -0600

    added logic to show state badge new fav link

--- src/components/main-nav/MainNavigation.js
+++ src/components/main-nav/MainNavigation.js
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import mainNavCss from "./MainNavigation.module.css";
+import FavoritesContext from "../../state-mgmt/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
     <header className={mainNavCss.header}>
       <div className={mainNavCss.logo}>Great MeetUps</div>
@@ -14,7 +17,12 @@ function MainNavigation() {
             <NavLink to="/new-meetup">New MeetUps</NavLink>
           </li>
           <li>
-            <NavLink to="/favorites">My Favorites</NavLink>
+            <NavLink to="/favorites">
+              Favorites
+              <span className={mainNavCss.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </NavLink>
           </li>
         </ul>
       </nav>
